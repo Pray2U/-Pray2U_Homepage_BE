fe6d238a85264f17dcfcce53ae097e5578de86ff
Dongbok-Lee <typoon0820@gmail.com>
2023-06-25 17:58:24 +0900

    프로젝트 생성

---
+++ p2uhomepage/src/test/java/com/pray2you/p2uhomepage/P2uhomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pray2you.p2uhomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2uhomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
